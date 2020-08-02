9c70e7a788efad3928f52da109a3f5094d798dc6
Rahul Vagadiya <rahul.vagadiya@gmail.com>
2020-08-02 13:18:35 +0400

    feat: FAQs and Promo Codes lookups

---
+++ stayhopper/admin/controllers/v2/promo-codes.js
@@ -0,0 +1,203 @@
+const express = require("express");
+const router = express.Router();
+const paginate = require("express-paginate");
+const config = require("config");
+
+const PromoCode = require("../../../db/models/promocodes");
+const jwtMiddleware = require("../../../middleware/jwt");
+
+
+// START: Customize
+const ModuleTitle = "Promotional Codes";
+const ModuleModel = PromoCode;
+const selections = '';
+const populations = '';
+
+const hasPermissions = (req, res) => {
+  // Permissions
+  let user = req.user;
+  let {permissions} = user.role;
+  const hasResourceAccess = permissions.indexOf(config.permissions.SHOW_SETTINGS) > -1;
+  if (hasResourceAccess) {
+    return true;
+  } else {
+    res.status(401).send({
+      message: 'Sorry, you do not have access to this resource'
+    }).end();
+    return false;
+  } 
+}
+
+const prepareQueryForListing = (req) => {
+  const where = {};
+  return where;
+}
+
+const preCreateOrUpdate = async (req, res, resourceData) => {
+  // Do nothing
+  return resourceData;
+}
+// END: Customize
+
+
+// Generic Listing+CRUD
+const list = async (req, res) => {
+  // if (hasPermissions(req, res)) {
+    try {
+      // Where condition
+      const where = prepareQueryForListing(req);
+
+      // Pagination
+      let active_page = 1;
+      if (req.query.page) {
+        active_page = req.query.page;
+      }
+
+      // Sorting
+      let sort = { _id: 1 };
+      if (req.query.order && req.query.orderBy) {
+        sort = {};
+        sort[req.query.orderBy] = req.query.order === 'asc' ? 1 : -1;
+      }
+
+      const [list, itemCount] = await Promise.all([
+        ModuleModel
+          .find(where)
+          .select(selections || '')
+          .populate(populations || '')
+          .sort(sort)
+          .limit(req.query.limit)
+          .skip(req.skip)
+          .lean()
+          .exec(),
+        ModuleModel.countDocuments(where)
+      ]);
+
+      const pageCount = Math.ceil(itemCount / req.query.limit);
+      let data = {
+        list: list,
+        itemCount: itemCount,
+        pageCount: pageCount,
+        pages: paginate.getArrayPages(req)(10, pageCount, req.query.page),
+        active_page
+      };
+      res.status(200).send(data).end();
+    } catch (e) {
+      console.log('e', e);
+      res.status(500).send({
+        message: 'Sorry, there was an error in performing this action'
+      }).end();
+    }
+  // }
+};
+
+const single = async (req, res) => {
+  if (hasPermissions(req, res)) {
+    try {
+      let where = {_id: req.params.id};
+      const [resource] = await Promise.all([
+        ModuleModel
+          .findOne(where)
+          .select(selections)
+          .populate(populations)
+          .lean()
+          .exec()
+      ]);
+
+      if (!resource) {
+        return res.status(404).send({
+          message: `${ModuleTitle} does not exist`
+        }).end();
+      }
+
+      res.status(200).send(resource).end();
+    } catch (e) {
+      return res.status(500).send({
+        message: 'Sorry, there was an error in performing this action'
+      }).end();
+    }
+  }
+};
+
+const create = async (req, res) => {
+  if (hasPermissions(req, res)) {
+    try {
+      // Pre call could be anything to be done before a POST / PUT is performed
+      // This could also be empty
+      let resourceData = req.body;
+      resourceData = await preCreateOrUpdate(req, res, resourceData);
+
+      const resource = new ModuleModel(resourceData);
+      await resource.save()
+      await ModuleModel.populate(resource, populations);
+
+      if (resource) {
+        res.status(200).send(resource).end();
+      } else {
+        res.status(500).send({
+          message: 'Sorry, there was an error in performing this action'
+        }).end();
+      }
+    } catch (e) {
+      console.log('e', e);
+      res.status(500).send({
+        message: 'Sorry, there was an error in performing this action',
+        error: e
+      }).end();
+    }
+  }
+}
+
+const modify = async (req, res) => {
+  if (hasPermissions(req, res)) {
+    try {
+      // Pre call could be anything to be done before a POST / PUT is performed
+      // This could also be empty
+      let resourceData = req.body;
+      resourceData = await preCreateOrUpdate(req, res, resourceData);
+
+      let where = {_id: req.params.id};  
+      const resource = await ModuleModel.findOne(where);
+      if (resource) {
+        Object.keys(resourceData).map(key => resource[key] = resourceData[key]);
+        await resource.save();
+        await ModuleModel.populate(resource, populations);
+
+        res.status(200).send(resource).end();
+      } else {
+        res.status(404).send({
+          message: 'Sorry, resource does not exist'
+        }).end();
+      }
+    } catch (e) {
+      console.log('e', e);
+      res.status(500).send({
+        message: 'Sorry, there was an error in performing this action'
+      }).end();
+    }
+  }
+}
+
+const remove = async (req, res) => {
+  if (hasPermissions(req, res)) {
+    let where = {_id: req.params.id};
+    try {
+      // Return updated doc
+      const resource = await ModuleModel.deleteOne(where).exec();
+      res.status(200).send(resource).end();
+    } catch (e) {
+      console.log('e', e);
+      res.status(500).send({
+        message: 'Sorry, there was an error in performing this operation'
+      }).end();
+    }
+  }
+}
+
+router.get("/", jwtMiddleware.administratorAuthenticationRequired, paginate.middleware(10, 100), list);
+router.get("/:id", jwtMiddleware.administratorAuthenticationRequired, paginate.middleware(10, 100), single);
+router.post("/", jwtMiddleware.administratorAuthenticationRequired, create);
+router.put("/:id", jwtMiddleware.administratorAuthenticationRequired, modify);
+router.delete("/:id", jwtMiddleware.administratorAuthenticationRequired, remove);
+
+module.exports = router;
